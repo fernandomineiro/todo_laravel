c0ac3afe9bb138764b32ffd24421ae116333ea75
Fernando Fernandes <fernando@parceirogoogle.com.br>
2024-07-24 10:43:05 -0300

    up

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -11,9 +11,13 @@ class LogController extends Controller
     public function index()
     {
         // Buscar todos os logs
+        if (Auth::check() && Auth::user()->role === 'admin') {
         $logs = Log::with('user')->get(); // Usa a relação definida no modelo
  
         return view('logs.index', compact('logs'));
+    }
+
+    return redirect('/home')->with('error', 'Você não tem permissão para acessar esta página.');
         // return response()->json($logs);
     }
 }
